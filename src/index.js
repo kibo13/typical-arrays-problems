0ff687ccc2b55b4100bc710f2baaef7235bbc561
Boris Kim <kimboris1310@gmail.com>
2021-02-17 19:42:43 +0500

    feat: complete typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+exports.min = function min(array) {
+    return array && array.length > 0 ? Math.min.apply(null, array) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return array && array.length > 0 ? Math.max.apply(null, array) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return array && array.length > 0
+        ? array.reduce((sum, current) => sum + current) / array.length
+        : 0;
+};
